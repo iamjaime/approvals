3593ecb117d07414b9b74ba36eb53e2020c56dab
Code Head <codehead@codelegends.com>
2018-11-26 13:31:47 -0500

    added eloquent relations

--- src/Models/Approval.php
+++ src/Models/Approval.php
@@ -3,8 +3,25 @@
 namespace Httpfactory\Approvals\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Httpfactory\Approvals\Models\Tag;
 
 class Approval extends Model
 {
-    //
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+
+    /**
+     * The tags associated with this approval
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
+
 }
--- src/Models/ApprovalConfiguration.php
+++ src/Models/ApprovalConfiguration.php
@@ -3,8 +3,24 @@
 namespace Httpfactory\Approvals\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Httpfactory\Approvals\Models\Tag;
+
 
 class ApprovalConfiguration extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+
+    /**
+     * The tags associated with this approval configuration
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
 }
