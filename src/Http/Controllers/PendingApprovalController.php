e51d5f74c9172a867d9915e97be2a41f8c3c0a03
Code Head <codehead@codelegends.com>
2018-11-28 16:19:27 -0500

    added pending approval action views

--- src/Http/Controllers/PendingApprovalController.php
+++ src/Http/Controllers/PendingApprovalController.php
@@ -2,7 +2,7 @@
 
 namespace Httpfactory\Approvals\Http\Controllers;
 
-use Httpfactory\Approvals\Models\Approval;
+use Httpfactory\Approvals\Models\Approver;
 
 class PendingApprovalController extends Controller
 {
@@ -11,29 +11,35 @@ class PendingApprovalController extends Controller
      * Handles approving the Approval.
      *
      * @param $token
+     * @return mixed
      */
     public function approve($token)
     {
-        $approvalRecord = Approval::where('token', '=', $token)->first();
+        $approvalRecord = Approver::where('token', '=', $token)->first();
         abort_unless($approvalRecord, 404);
         $approvalRecord->status = 'approved';
+        $approvalRecord->token = '';
         $approvalRecord->save();
 
         //now we should redirect somewhere...
+        return view('approvals::approvals.approve');
     }
 
     /**
      * Handles denying the Approval.
      *
      * @param $token
+     * @return mixed
      */
     public function deny($token)
     {
-        $approvalRecord = Approval::where('token', '=', $token)->first();
+        $approvalRecord = Approver::where('token', '=', $token)->first();
         abort_unless($approvalRecord, 404);
         $approvalRecord->status = 'denied';
+        $approvalRecord->token = '';
         $approvalRecord->save();
 
         //now we should redirect somewhere...
+        return view('approvals::approvals.denied');
     }
 }
