8024918430cd525242882e71ba6f067ca54147f6
Code Head <codehead@codelegends.com>
2018-11-29 13:36:43 -0500

    added approval awarded sample listener

---
+++ src/Listeners/ApprovalAwarded.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Httpfactory\Approvals\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use Httpfactory\Approvals\Models\Approval;
+
+class ApprovalAwarded
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //get the approval object from the event....
+        $approval = $event->approval;
+
+        //we should be able to grab the requester's user instance like so
+        $requester = $approval->requester;
+
+        //here we should probably shoot an email to the requester of the approval
+        //this is just an example listener because whoever uses this package should be able
+        //to add their own listener for the events that we fire off.
+    }
+}
