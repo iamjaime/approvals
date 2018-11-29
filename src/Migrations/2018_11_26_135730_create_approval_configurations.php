b8338c89bd81486ff393509a80ef11fca0e19c7a
Code Head <codehead@codelegends.com>
2018-11-29 09:35:07 -0500

    added approval config id to approvals table

--- src/Migrations/2018_11_26_135730_create_approval_configurations.php
+++ src/Migrations/2018_11_26_135730_create_approval_configurations.php
@@ -15,11 +15,25 @@ class CreateApprovalConfigurations extends Migration
     {
         Schema::create('approval_configurations', function (Blueprint $table) {
             $table->increments('id');
+
+            //Foreign Key Referencing the id on the approvals table.
+            $table->integer('approval_id')->unsigned();
+            $table->foreign('approval_id')->references('id')->on('approvals')->onDelete('cascade');
+
             $table->string('name');
             $table->text('description');
             $table->integer('yes'); //number of yes's required before auto approval
             $table->integer('no'); //number of no's required before auto denial
             $table->timestamps();
+        });
+
+
+        Schema::table('approvals', function (Blueprint $table) {
+
+            //Foreign Key Referencing the id on the approvals table.
+            $table->integer('approval_config_id')->after('requester_id')->unsigned();
+            $table->foreign('approval_config_id')->references('id')->on('approval_configurations')->onDelete('cascade');
+
         });
     }
 
@@ -30,6 +44,14 @@ class CreateApprovalConfigurations extends Migration
      */
     public function down()
     {
+        Schema::table('approval_configurations', function($table) {
+            $table->dropForeign(['approval_id']);
+        });
+
+        Schema::table('approvals', function($table) {
+            $table->dropForeign(['approval_config_id']);
+        });
+
         Schema::dropIfExists('approval_configurations');
     }
 }
