5b626233bd50371957151713a3053e04e5900c0a
Code Head <codehead@codelegends.com>
2018-11-30 07:53:28 -0500

    added awarded and denied logic

--- src/Repositories/ApprovalRepository.php
+++ src/Repositories/ApprovalRepository.php
@@ -2,7 +2,10 @@
 
 namespace Httpfactory\Approvals\Repositories;
 
+use Httpfactory\Approvals\Events\ApprovalAwarded;
+use Httpfactory\Approvals\Listeners\ApprovalDenied;
 use Httpfactory\Approvals\Models\Approver;
+use Httpfactory\Approvals\Models\Approval;
 use Httpfactory\Approvals\Contracts\ApprovalRepository as ApprovalRepositoryInterface;
 use Httpfactory\Approvals\Events\ApprovalApproved;
 use Httpfactory\Approvals\Events\ApprovalDeclined;
@@ -21,7 +24,7 @@ class ApprovalRepository implements ApprovalRepositoryInterface
         //The user should have clicked on the email call to action button and
         //that will trigger this method.
         //at this point, we will query the database and update the record to approved
-        $approvalRecord = Approver::where('token', '=', $token)->with(['approval', 'approver'])->first();
+        $approvalRecord = Approver::where('token', '=', $token)->with(['approval.configuration', 'approver'])->first();
         $approvalRecord->status = 'approved';
         $approvalRecord->token = '';
         $approvalRecord->save();
@@ -29,6 +32,8 @@ class ApprovalRepository implements ApprovalRepositoryInterface
         //then fire off some event which indicates approval approved
         $approvedBy = $approvalRecord->approver;
         event(new ApprovalApproved($approvedBy));
+
+        $this->finalizeApproval($approvalRecord);
 
         return $approvalRecord;
     }
@@ -41,7 +46,7 @@ class ApprovalRepository implements ApprovalRepositoryInterface
      */
     public function decline($token)
     {
-        $approvalRecord = Approver::where('token', '=', $token)->with(['approval', 'approver'])->first();
+        $approvalRecord = Approver::where('token', '=', $token)->with(['approval.configuration', 'approver'])->first();
         $approvalRecord->status = 'declined';
         $approvalRecord->token = '';
         $approvalRecord->save();
@@ -51,6 +56,8 @@ class ApprovalRepository implements ApprovalRepositoryInterface
         $declinedBy = $approvalRecord->approver;
 
         event(new ApprovalDeclined($approval, $declinedBy));
+
+        $this->finalizeApproval($approvalRecord);
 
         return $approvalRecord;
     }
@@ -69,5 +76,52 @@ class ApprovalRepository implements ApprovalRepositoryInterface
             return true;
         }
         return false;
+    }
+
+
+    /**
+     * Handle awarding the approval
+     * @param $approval
+     */
+    protected function award($approval)
+    {
+        $approvalRecord = Approval::where('id', '=', $approval->id)->first();
+        $approvalRecord->status = 'awarded';
+        $approvalRecord->save();
+
+        event(new ApprovalAwarded($approval));
+    }
+
+    /**
+     * Handle denying the approval
+     * @param $approval
+     */
+    protected function denied($approval)
+    {
+        $approvalRecord = Approval::where('id', '=', $approval->id)->first();
+        $approvalRecord->status = 'denied';
+        $approvalRecord->save();
+
+        event(new ApprovalDenied($approval));
+    }
+
+
+    /**
+     * Handles finalizing the approval
+     *
+     * @param $approval
+     */
+    protected function finalizeApproval($approval)
+    {
+       $config = $approval->configuration();
+       $approvedRecords = Approver::where('approval_id', '=', $approval->id)->where('status', '=', 'approved')->count();
+       $declinedRecords = Approver::where('approval_id', '=', $approval->id)->where('status', '=', 'declined')->count();
+
+       if( $config->yes >= $approvedRecords ){
+           $this->award($approval);
+       }elseif( $config->no >= $declinedRecords ){
+           $this->denied($approval);
+       }
+
     }
 }
