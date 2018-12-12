acc3580441f445f4e4dccf106bab10c1308496fb
Code Head <codehead@codelegends.com>
2018-12-12 13:10:30 -0500

    created basic views for creating groups and updating groups

---
+++ src/Views/approvals/groups/update-group.blade.php
@@ -0,0 +1,24 @@
+<h2>Update Group</h2>
+<!-- Begin Update Group Section -->
+<section>
+
+    <form action="">
+
+        <div class="form-group">
+            <label>Group Name</label>
+            <input type="text" name="name">
+        </div>
+
+        <div class="form-group">
+            <label>Group Description</label>
+            <input type="text" name="Description">
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Create Group">
+        </div>
+
+    </form>
+
+</section>
+<!-- End Update Group Section -->
