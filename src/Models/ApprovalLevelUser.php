2de3b601ef9d1ca997f67e05c29ef217053d19c2
Code Head <codehead@codelegends.com>
2019-01-14 13:40:27 -0500

    refactored approval level user

--- src/Models/ApprovalLevelUser.php
+++ src/Models/ApprovalLevelUser.php
@@ -4,6 +4,7 @@ namespace Httpfactory\Approvals\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Httpfactory\Approvals\Models\ApprovalLevel;
+use Httpfactory\Approvals\Models\ApprovalLevelRequest;
 
 class ApprovalLevelUser extends Model
 {
@@ -19,6 +20,17 @@ class ApprovalLevelUser extends Model
     public function approvalLevel()
     {
         return $this->belongsTo(ApprovalLevel::class);
+    }
+
+
+    /**
+     * Get the approval requests associated with this approval level user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function approvalRequests()
+    {
+        return $this->hasMany(ApprovalLevelRequest::class);
     }
 
 }
