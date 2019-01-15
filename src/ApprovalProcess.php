8519a94c7b25e84299e31654b784d3efb8a2a5f6
Code Head <codehead@codelegends.com>
2019-01-15 13:47:57 -0500

    added create process with element method

--- src/ApprovalProcess.php
+++ src/ApprovalProcess.php
@@ -4,9 +4,23 @@ namespace Httpfactory\Approvals;
 
 use Httpfactory\Approvals\Repositories\ApprovalProcessRepository;
 
-
 class ApprovalProcess extends ApprovalProcessRepository {
 
 
+    /**
+     * Handles creating an approval process with an element at the same time
+     *
+     * @param $data
+     * @param $team_id
+     * @param $element \Httpfactory\Approvals\Models\ApprovalElement
+     */
+    public function createWithElement($data, $team_id, $element)
+    {
+        $process = $this->create($data, $team_id);
+        $element->team_id = $team_id;
+        $el = $process->approvalElement()->save($element);
+        $data['approval_element_id'] = $el->id;
+        $this->update($data, $process->id);
+    }
 
 }
