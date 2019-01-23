70f6845a52b09576b4c58f047794edd95e79f797
Code Head <codehead@codelegends.com>
2019-01-23 17:13:53 -0500

    updated approval request emails

--- src/Views/Emails/approval-request.blade.php
+++ src/Views/Emails/approval-request.blade.php
@@ -2,13 +2,13 @@
 
 <br><br>
 
-you are receiving this email because {{ $approval->requester->name }} has requested an approval from you.
+you are receiving this email because {{ $approval->requester->name }} has requested an approval from {{ $approval->currentLevel->name  }}.
 
 <br><br>
 
-{{ $approval->name }} <br>
+{{ $approval->approvalProcess->name }} <br>
 
-{{ $approver->description }} <br><br>
+{{ $approval->approvalProcess->description }} <br><br>
 
-<a href="{{ env('APP_URL') . '/approve/' . $approver->token }}">Approve</a> / <a href="{{ env('APP_URL') . '/deny/' . $approver->token }}">Deny</a>
+<a href="{{ env('APP_URL') . '/approve/' . $approver->approval_level_request->token }}">Approve</a> / <a href="{{ env('APP_URL') . '/deny/' . $approver->approval_level_request->token }}">Deny</a>
 
