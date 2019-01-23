55bb42c23d2356ada1ac42e8c76cd2bdffb51293
Code Head <codehead@codelegends.com>
2019-01-23 10:27:19 -0500

    updated abstract approval class

--- src/Repositories/Approval.php
+++ src/Repositories/Approval.php
@@ -7,6 +7,7 @@ use Httpfactory\Approvals\Events\ApprovalRequest;
 use Httpfactory\Approvals\Models\ApprovalRequest as ApprovalRecord;
 use Httpfactory\Approvals\Models\ApprovalLevelRequest as Approver;
 use Httpfactory\Approvals\Models\ApprovalProcess;
+use Httpfactory\Approvals\Models\ApprovalLevel;
 use Httpfactory\Approvals\Models\User;
 
 use Illuminate\Contracts\Auth\Authenticatable as ApprovalRequester;
@@ -35,6 +36,20 @@ abstract class Approval implements Approvable {
      */
     public $levels;
 
+    /**
+     * The approval request object
+     *
+     * @var \Httpfactory\Approvals\Models\ApprovalRequest
+     */
+    public $approvalRequest;
+
+    /**
+     * The current assessment level with users associated to it.
+     *
+     * @var \Httpfactory\Approvals\Models\ApprovalLevel
+     */
+    public $currentLevel;
+
 
     public function __construct(ApprovalRequester $requester, ApprovalProcess $approvalProcess)
     {
@@ -55,6 +70,8 @@ abstract class Approval implements Approvable {
      */
     public function sendRequest()
     {
+        $this->saveApprovalRequest();
+
         //fires an event
         event(new ApprovalRequest($this));
     }
@@ -80,5 +97,41 @@ abstract class Approval implements Approvable {
 
     }
 
+
+    /**
+     * Handles saving the approval request
+     */
+    protected function saveApprovalRequest()
+    {
+        $approvalRequest = new ApprovalRecord();
+        $approvalRequest->team_id = $this->team->id;
+        $approvalRequest->requester_id = $this->requester->id;
+        $approvalRequest->approval_process_id = $this->approvalProcess->id;
+        $approvalRequest->current_assessment_level = $this->levels[0]->id; //initial level
+        $approvalRequest->status = "pending";
+        $approvalRequest->save();
+
+        $this->approvalRequest = $approvalRequest;
+
+        $this->getCurrentAssessmentApprovalLevelUsers();
+    }
+
+
+    /**
+     * Handles getting the current approval level users
+     */
+    private function getCurrentAssessmentApprovalLevelUsers()
+    {
+        $userIds = [];
+        $currentLevel = ApprovalLevel::where('id', $this->approvalRequest->current_assessment_level)->with('users')->first();
+        foreach($currentLevel->users as $user){
+            array_push($userIds, $user->user_id);
+        }
+
+        $users = User::whereIn('id', $userIds)->get();
+
+        $this->currentLevel = $currentLevel;
+        $this->currentLevel->users = $users;
+    }
 
 }
