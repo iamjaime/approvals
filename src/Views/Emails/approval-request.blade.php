eef231197b65db00c6eaa0c84ebbf6ed35294966
Code Head <codehead@codelegends.com>
2019-02-07 14:26:55 -0500

    fixed the approval request blade view

--- src/Views/Emails/approval-request.blade.php
+++ src/Views/Emails/approval-request.blade.php
@@ -10,5 +10,5 @@ you are receiving this email because {{ $approval->requester->name }} has reques
 
 {{ $approval->approvalProcess->description }} <br><br>
 
-<a href="{{ env('APP_URL') . '/approve/' . $approver->approval_level_request->token }}">Approve</a> / <a href="{{ env('APP_URL') . '/deny/' . $approver->approval_level_request->token }}">Deny</a>
+<a href="{{ env('APP_URL') . '/approve/' . $approver->approval_level_request->token }}">Approve</a> / <a href="{{ env('APP_URL') . '/decline/' . $approver->approval_level_request->token }}">Deny</a>
 
