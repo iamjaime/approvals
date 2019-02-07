8a213a84f07d58e944d4f10417585dfa43653ab2
Code Head <codehead@codelegends.com>
2019-02-07 14:29:05 -0500

    added approval request relationship to the level request model

--- src/Models/ApprovalLevelRequest.php
+++ src/Models/ApprovalLevelRequest.php
@@ -3,8 +3,19 @@
 namespace Httpfactory\Approvals\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Httpfactory\Approvals\Models\ApprovalRequest;
 
 class ApprovalLevelRequest extends Model
 {
-    //
+
+
+    /**
+     * Gets the direct approval request associate with this level request
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function approvalRequest()
+    {
+        return $this->hasOne(ApprovalRequest::class, 'id', 'approval_request_id');
+    }
 }
